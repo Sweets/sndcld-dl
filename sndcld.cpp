384d34d1167d9838f0491e645792b2387575afea
スーイツ <Sweets@users.noreply.github.com>
2017-09-13 01:41:51 -0500

    Update sndcld.cpp

--- sndcld.cpp
+++ sndcld.cpp
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <iostream>
 #include <fstream>
 #include <regex>
 
@@ -8,139 +9,227 @@
 #include <curlpp/Options.hpp>
 #include <json11.hpp>
 
-void print(std::string string) {
-    std::cout << string << std::endl;
-} // BECAUSE I'M LAZY
+// HERE BE DRAGONS
 
-std::string GET(std::string url) {
-    curlpp::Cleanup cleaner;
-    curlpp::Easy request;
+namespace sndcld_util {
 
-    request.setOpt(new curlpp::options::Url(url));
+    const std::string user_agent = "User-Agent: Mozilla/5.0";
 
-    std::list<std::string> headers;
-    headers.push_back("User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
+    std::string _replaceAll(std::string string, std::string substring, std::string replacement, unsigned int index) {
 
-    request.setOpt(new curlpp::options::HttpHeader(headers));
+        if (string.find(substring, index) == std::string::npos) {
+            return string;
+        } else {
+            return _replaceAll(
+                string.replace(
+                    string.find(substring, index),
+                    replacement.length(),
+                    replacement
+                ),
+                substring,
+                replacement,
+                string.find(
+                    substring, index
+                )
+            );
+        }
 
-    std::stringstream data;
-    data << request;
-    const std::string response = data.str();
-    return response;
-}
-
-std::string replaceAll(std::string string, std::string substring, std::string replacement, int index) {
-    if (string.find(substring, index) == std::string::npos) {
-        return string;
-    } else {
-        return replaceAll(string.replace(string.find(substring, index), replacement.length(), replacement), substring, replacement, string.find(substring, index));
-    }
-}
-
-std::string concat(std::vector<std::string> vector, int start, int end, std::string concatenator) {
-    if (start < 0 || vector.size() - 1 < end)
-        return "";
-    std::string string;
-    for (int index = start; index <= end; index++) {
-        string += vector.at(index) + concatenator;
-    }
-    return string;
-}
-
-void download(std::string path, std::string url) {
-    curlpp::Cleanup cleaner;
-    curlpp::Easy request;
-
-    request.setOpt(new curlpp::options::Url(url));
-
-    std::list<std::string> headers;
-    headers.push_back("User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
-
-    request.setOpt(new curlpp::options::HttpHeader(headers));
-
-    std::ofstream file;
-    print(" [♥] Path opened, streaming file ");
-    file.open(path);
-    file << request;
-    file.close();
-    print(" [♥] Path closed. MP3 dowloaded. ");
-}
-
-std::string getMalformedURL(std::string playlistData) {
-
-    print(" [♥] Getting mp3 size ");
-    std::stringstream playlistStringStream(playlistData);
-
-    std::vector<std::string> lines;
-    std::string line;
-    while (playlistStringStream >> line)
-        lines.push_back(line);
-
-    std::stringstream urlStream(replaceAll(lines.at(lines.size() - 2), "/", "\n", 0));
-    std::vector<std::string> urlParts;
-    std::string part;
-    while (urlStream >> part)
-        urlParts.push_back(part);
-
-    print(" [♥] Concatenating URL parts ");
-    std::string malformedUrl = concat(urlParts, 0, 2, "/") + "0/" + urlParts.at(4) + "/" + urlParts.at(5);
-
-    return malformedUrl;
-}
-
-std::string getPlaylist(long int trackId) {
-    print(" [♥] Sending stream GET ");
-    const std::string url = "https://api.soundcloud.com/i1/tracks/" + std::to_string(trackId) +
-        "/streams?client_id=" +
-        "2t9loNQH90kzJcsFCODdigxfp325aq4z" + // client ID
-        "&app_version=" +
-        std::to_string(
-            1496423089
-        ); // appversion
-
-    // If you yell at me for concatenating client ID, I will kill you so hard you die to death.
-    // It's concatenated rather than hardcoded into the string after the track ID because it makes it
-    // easier to edit.Gettig stream playlist
-
-    const std::string response = GET(url);
-    print(" [♥] Parsing stream data ");
-    std::string JSONError;
-    const json11::Json data = json11::Json::parse(response, JSONError);
-    print(" [♥] Data parsed. Constructing playlist URL ");
-    const std::string playlisturl = data["hls_mp3_128_url"].string_value();
-    print(" [♥] Sending playlist GET ");
-    const std::string playlist = GET(playlisturl);
-    print(" [♥] Got playlist ");
-
-    return playlist;
-}
-
-int main(int argc, char *argv[]) {
-    std::string url(argv[1]);
-    std::string path(argv[2]);
-
-    print(" [♥] sndcld v2, written by Sweets ");
-
-    print(" [♥] Sending GET request ");
-    const std::string response = GET(url);
-    print(" [♥] Searching for track ID ");
-
-    std::regex json("publisher_metadata\":\\{(.*?)\\},\"purchase_title");
-    std::smatch match;
-
-    if (std::regex_search(response, match, json)) {
-        std::string strMatch(match[0]);
-        print(" [♥] Parsing publisher_metadata JSON for ID ");
-        std::string JSONError;
-        const json11::Json metadata = json11::Json::parse(strMatch.substr(20, strMatch.length() - 36), JSONError);
-        long int id = std::stol(metadata["id"].dump());
-        print(" [♥] ID found => " + std::to_string(id));
-        print(" [♥] Gettig stream playlist ");
-        std::string playlist = getPlaylist(id);
-        print(" [♥] Constructing malformed URL for mp3 file ");
-        std::string mp3url = getMalformedURL(playlist);
-        download(path, mp3url);
     }
 
-    return 0;
+    std::string replaceAll(std::string string, std::string substring, std::string replacement) {
+
+        return _replaceAll(
+            string, substring, replacement, 0
+        );
+
+    }
+
+    std::string GET(std::string url, std::string output_path) {
+        curlpp::Cleanup cleaner;
+        curlpp::Easy request;
+
+        request.setOpt(new curlpp::options::Url(url));
+
+        std::list<std::string> headers;
+        headers.push_back(user_agent);
+
+        request.setOpt(new curlpp::options::HttpHeader(headers));
+
+        if (output_path == "") {
+
+            std::stringstream data;
+            data << request;
+
+            return data.str();
+
+        } else {
+
+            std::ofstream file;
+            file.open(output_path);
+            file << request;
+            file.close();
+
+            return output_path;
+
+        }
+
+        return ""; // Never reached
+
+    }
+
+}
+
+namespace sndcld {
+
+    std::string client_id;
+    long int appversion;
+
+    void set_params(std::string cid, long int av) {
+        client_id = cid;
+        appversion = av;
+        // The way those lines aligned is so satisfying
+    }
+
+    namespace track_id_methods {
+
+        long int fallback_1(std::string url) {
+
+            return -1; // -1 means it can't be found, even after any fallbacks.
+
+        }
+
+        long int main(std::string url) {
+
+            const std::string response = sndcld_util::GET(url, "");
+
+            std::regex json("\"uri\":\"https://api.soundcloud.com/tracks/(\\d+)\"");
+            std::smatch matches;
+
+            if (std::regex_search(response, matches, json)) {
+                long int id = std::stoi(matches[1]);
+                return id;
+            } else {
+                return fallback_1(url);
+            }
+
+        }
+
+    } // End track id methods
+
+    long int get_track_id(std::string url) {
+
+        return track_id_methods::main(url);
+
+    }
+
+    std::string get_playlist(long int track_id) {
+
+        if (track_id == -1)
+            return "";
+
+        const std::string streams_url = "https://api.soundcloud.com/i1/tracks/" + std::to_string(track_id) +
+            "/streams?client_id=" + client_id + "&app_version=" + std::to_string(appversion);
+
+        const std::string response = sndcld_util::GET(streams_url, "");
+        std::string ERROR = "";
+        const json11::Json JSON = json11::Json::parse(response, ERROR);
+
+        if (ERROR != "")
+            return "";
+
+        const std::string playlist_url = JSON["hls_mp3_128_url"].string_value();
+        const std::string playlist = sndcld_util::GET(playlist_url, "");
+
+        return playlist;
+
+    }
+
+    std::string get_malformed_url(std::string playlist_str) {
+
+        if (playlist_str == "")
+            return "";
+
+        std::stringstream playlist(playlist_str);
+
+        std::vector<std::string> lines;
+        std::string line;
+
+        while (playlist >> line)
+            lines.push_back(line);
+
+        std::stringstream url(
+
+            sndcld_util::replaceAll(
+                lines.at(lines.size() - 2),
+                "/", "\n"
+            )
+
+        );
+
+        std::vector<std::string> url_parts;
+        std::string url_part;
+
+        while (url >> url_part)
+            url_parts.push_back(url_part);
+
+        std::string malformed_url = url_parts.at(0) + "/" + url_parts.at(1) + "/" + url_parts.at(2) + "/0/" + url_parts.at(4) + "/" + url_parts.at(5);
+
+        return malformed_url;
+
+    }
+
+} // End sndcld
+
+void usage(void) {
+
+    printf("%s\n", "sndcld v3 by Sweets");
+    printf("%s\n", " Usage: sndcld [artist] [title] [output path]");
+    printf("%s\n", " Example: sndcld iameden r-r ~/Music/eden-rock-and-roll.mp3");
+    printf("%s\n", " URL for song in example: https://soundcloud.com/iameden/r-r");
+
+}
+
+int main(int argc, char **argv) {
+
+    if (argv[1] == NULL || argv[2] == NULL || argv[3] == NULL || argv[4] != NULL) {
+        usage();
+        return 1;
+    }
+
+    std::string artist(argv[1]);
+    std::string title(argv[2]);
+    std::string output_path(argv[3]);
+    
+    printf("%s\n", " [❤] sndcld v3 by Sweets");
+
+    std::string url = "https://soundcloud.com/" + artist + "/" + title;
+
+    printf("%s\n", " [❤] Setting client_id and appversion.");
+    sndcld::set_params("WKcQQdEZw7Oi01KqtHWxeVSxNyRzgT8M", 1505226596);
+    printf("%s\n", " [❤] Getting track_id.");
+
+    try {
+        long int track_id = sndcld::get_track_id(url);
+        printf("%s\n", " [❤] Got track_id.");
+
+        printf("%s\n", " [❤] Getting playlist data.");
+        std::string playlist = sndcld::get_playlist(track_id);
+        printf("%s\n", " [❤] Got playlist data.");
+
+        printf("%s\n", " [❤] Creating malformed URL.");
+        std::string mp3url = sndcld::get_malformed_url(playlist);
+        printf("%s\n", " [❤] Malformed URL created.");
+
+        printf("%s\n", " [❤] Downloading song.");
+        sndcld_util::GET(mp3url, output_path);
+        printf("%s\n", " [❤] Sond downloaded.");
+
+        return 0;
+
+    } catch (curlpp::LibcurlRuntimeError) {
+        printf("%s\n", " [(' ^ ';)] Unable to resolve host.");
+
+        return 1;
+    }
+
 }
